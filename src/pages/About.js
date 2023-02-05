00068ed6edf343ba053707e9e8f8e00a720bfd85
novenopatch <homawoojoseph@gmail.com>
2023-02-05 08:44:49 +0000

    country app with react

---
+++ src/pages/About.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from "../components/Navigation";
+
+
+const About = () => {
+    return (
+        <div>
+            <Logo/>
+            <Navigation/>
+          <h1>A propos</h1> 
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit tempora eius eum optio ratione reiciendis? Sit ipsa officiis veniam? Sed quae molestias reiciendis in necessitatibus assumenda eius, fugiat eum voluptas quos accusantium. Ut, quod deleniti officiis architecto possimus cum ratione dignissimos sed corporis officia neque assumenda ea accusamus </p>
+          <br/>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi dolorum iusto in! Porro minima iste esse accusamus? Vero inventore adipisci ad expedita beatae cum, molestiae eaque eum! Similique eos molestias maiores odit assumenda tenetur iste error magnam maxime! Labore illum fuga possimus quo sequi hic enim quam distinctio alias, exercitationem, nulla obcaecati. Quod omnis porro rem asperiores qui voluptatibus repellendus? Voluptatum sint rerum culpa odio explicabo ab cumque suscipit praesentium similique vel eveniet natus, veniam sunt ipsam expedita possimus dolorem quia unde necessitatibus dolore nam minus beatae! Dolor error ea soluta? Ipsum deleniti non animi deserunt corporis alias incidunt blanditiis ratione voluptatum possimus voluptates a vero et sed atque ullam consequuntur voluptatem molestiae aut quibusdam quia, adipisci facilis mollitia iste. Nemo sunt facilis ut eligendi sit, enim quod fugit, quibusdam dolor inventore dolorem itaque? Quae velit aut, quod aliquid ex ea. Ad ab placeat, cum quos explicabo pariatur nam consequatur consectetur obcaecati, dignissimos nulla saepe. Enim neque praesentium quis sapiente magni ea, sed aut inventore suscipit obcaecati. Facere est eos saepe sequi commodi alias earum? Eius eveniet sint expedita impedit natus aspernatur, distinctio accusantium modi debitis ratione delectus sit officiis dicta ea minima optio aut totam iure commodi! Animi, laboriosam!
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis, est possimus quas tempora at culpa cumque quae, veritatis quam repudiandae ipsum expedita voluptatem tempore natus. Veritatis sequi molestiae quas dolor.
+            </p>
+            <br/>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos, ipsa, fugit, aliquid ab pariatur nisi iste reprehenderit cum dolor quisquam quae! Incidunt molestiae perspiciatis asperiores obcaecati nisi hic praesentium aspernatur explicabo, quam ut! Impedit architecto sint porro veniam quod dignissimos pariatur iusto! Obcaecati et voluptate veniam quasi, hic vitae, amet assumenda voluptatem porro odio id possimus est quod distinctio voluptates quaerat reiciendis cum tenetur aspernatur. Eaque magnam cupiditate ex sed iusto cumque expedita, assumenda esse cum voluptate tempora quisquam fuga aliquid repudiandae consequuntur! Quod, esse. Error velit ex quos nesciunt a veniam maiores reprehenderit inventore dolor, vero illo unde nemo.</p>
+        </div>
+    );
+};
+
+export default About;
